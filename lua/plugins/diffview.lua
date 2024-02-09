8c919a328a28d2ab5d940abf4eed4b47bd5de285
stormZhu <779724979@qq.com>
2024-02-10 00:53:24 +0800

    Create diffview.lua

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,17 @@
+return {
+  "sindrets/diffview.nvim",
+  event = "VeryLazy",
+  cmd = { "DiffviewOpen" },
+  opts = {
+    enhanced_diff_hl = true,
+    view = {
+      default = { winbar_info = true },
+      file_history = { winbar_info = true },
+    },
+    hooks = {
+      diff_buf_read = function(bufnr)
+        vim.b[bufnr].view_activated = false
+      end,
+    },
+  },
+}
