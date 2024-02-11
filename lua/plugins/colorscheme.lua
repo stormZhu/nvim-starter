72a482f18f40f2b3005d41617b4d4e2fe0f3ddef
stormZhu <779724979@qq.com>
2024-02-11 11:50:26 +0800

    Update colorscheme.lua
    修改配色

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,8 +3,23 @@ return {
   { "ellisonleao/gruvbox.nvim" },
   { 
     "tanvirtin/monokai.nvim",
-    opts = {
-      colorscheme = "monokai_pro",
-    },
+    opts = function()
+      local palette = require("monokai").pro
+
+      return {
+        palette = {
+          pink = "#e32078",
+        },
+        custom_hlgroups = {
+          Visual = {
+            bg = palette.grey,
+          },
+        },
+        VisualNOS = {
+          bg = palette.base3,
+        },
+        colorscheme = "monokai_pro",
+      }
+    end,
   },
 }
