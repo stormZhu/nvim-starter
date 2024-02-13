e308750c50b746f2a88ac5905ae2b046d739a1df
stormZhu <779724979@qq.com>
2024-02-13 19:43:01 +0800

    Update nvim-gdb.lua

--- lua/plugins/nvim-gdb.lua
+++ lua/plugins/nvim-gdb.lua
@@ -1,16 +1,22 @@
 return {
   "sakhnik/nvim-gdb",
   config = function ()
-    -- vim.cmd([[ 
-    --     let g:nvimgdb_config_override = {
-    --     \ 'sign_current_line': '>',
-    --     \ }
-    --   ]])
+    vim.cmd([[
+      let g:nvimgdb_config_override = {
+        \ 'key_until':      ';u',
+        \ 'key_continue':   ';c',
+        \ 'key_next':       ';n',
+        \ 'key_step':       ';s',
+        \ 'key_finish':     ';f',
+        \ 'key_breakpoint': ';b',
+        \ 'key_eval':       '<f9>',
+      \ }
+    ]])
     GdbSessionInit = function ()
       local utils = require("config.utils")
       if utils.exists('.bps.txt') then
         vim.defer_fn(function()
-          vim.api.nvim_command(":Gdb source .bps.txt")   -- 自动加载断点
+          vim.api.nvim_command(":Gdb source .bps.txt")
         end, 300)
       end    
     end
@@ -20,7 +26,7 @@ return {
   end
 }
 
--- ~/.gdbinit
+
 -- define hook-quit
 --   shell rm .bps.txt
 --   save breakpoints .bps.txt
