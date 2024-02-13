3937b723a7e3e8b9bd0980dad9e52868d65c7083
stormZhu <779724979@qq.com>
2024-02-13 20:01:41 +0800

    Create utils.lua

---
+++ lua/config/utils.lua
@@ -0,0 +1,15 @@
+local M = {}
+
+-- file exist?
+M.exists = function(file)
+  local ok, err, code = os.rename(file, file)
+  if not ok then
+    if code == 13 then
+      -- Permission denied, but it exists
+      return true
+    end
+  end
+  return ok, err
+end
+
+return M
