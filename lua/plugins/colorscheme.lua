851ed251fde4bfc257550febcfb985b6ea0ede9c
stormZhu <779724979@qq.com>
2024-02-16 00:44:48 +0800

    Update colorscheme.lua
    增加bamboo主题

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,7 +2,23 @@ return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
   {
+    "ribru17/bamboo.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("bamboo").setup({
+        -- optional configuration here
+        highlights = {
+          -- make comments blend nicely with background, similar to other color schemes
+          ["@comment"] = { fg = "$grey" },
+        },
+      })
+      require("bamboo").load()
+    end,
+  },
+  {
     "tanvirtin/monokai.nvim",
+    lazy = false,
     config = function()
       local palette = require("monokai").pro
       palette.aqua = "#66d9ef" -- 修改一下pro的蓝色
@@ -19,7 +35,7 @@ return {
             bg = palette.base4,
           },
           Comment = {
-            fg = palette.base7,  -- 注释亮一点
+            fg = palette.base7,
             style = "italic",
           },
         },
