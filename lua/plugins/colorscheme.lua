a8389148458dcb2630bfc9621accd304dca70fcc
stormZhu <779724979@qq.com>
2024-02-15 21:00:51 +0800

    Update colorscheme.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,23 +3,25 @@ return {
   { "ellisonleao/gruvbox.nvim" },
   {
     "tanvirtin/monokai.nvim",
-    config = function()    
+    config = function()
       local palette = require("monokai").pro
-      palette.aqua = '#66d9ef'   -- 修改一下pro的蓝色 
+      palette.aqua = "#66d9ef" -- 修改一下pro的蓝色
       require("monokai").setup({
         palette = palette,
         custom_hlgroups = {
-            Visual = {
-              bg = palette.grey,
-              fg = palette.white,
-            },
-            VisualNOS = {
-              bg = palette.grey,
-              fg = palette.white,
-            },
+          Visual = {
+            bg = palette.grey,
+            fg = palette.white,
           },
-        }
-      )
+          VisualNOS = {
+            bg = palette.grey,
+            fg = palette.white,
+          },
+          CursorLine = {
+            bg = palette.base4,
+          },
+        },
+      })
     end,
   },
   -- {
