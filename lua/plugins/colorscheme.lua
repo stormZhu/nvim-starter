8412730f59f77f3b05eeefd3db77a7a03cce9da8
stormZhu <779724979@qq.com>
2024-02-17 23:13:40 +0800

    Update colorscheme.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -44,18 +44,18 @@ return {
     --   vim.cmd("colorscheme github_dark_dimmed")
     -- end,
   },
+  { "Mofiqul/vscode.nvim", lazy = false, priority = 1000, },
   {
     "askfiy/visual_studio_code",
-    lazy = false,
-    priority = 100,
-    config = function()
-      vim.cmd([[colorscheme visual_studio_code]])
-    end,
+    -- priority = 100,
+    -- config = function()
+    --   vim.cmd([[colorscheme visual_studio_code]])
+    -- end,
   },
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "github_dark_dimmed", 
-  --   },
-  -- },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vscode",
+    },
+  },
 }
