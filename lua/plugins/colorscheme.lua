1e885f1bcd2d44adcf56347b6e9c5589bcb6a3b0
stormZhu <779724979@qq.com>
2024-02-17 21:19:07 +0800

    Update colorscheme.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,68 +1,53 @@
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
   {
     "tanvirtin/monokai.nvim",
     lazy = false,
+    -- config = function()
+    --   local palette = require("monokai").pro
+    --   palette.white = "#C3C0C0"
+    --   palette.grey = "#72696A"
+    --   palette.black = "#000000"
+    --   palette.pink = "#D06A86"
+    --   palette.green = "#77C269"
+    --   palette.aqua = "#6CB5CC"
+    --   palette.yellow = "#E4C15A"
+    --   palette.orange = "#FC9867"
+    --   palette.purple = "#AB9DF2"
+    --   palette.red = "#D15454"
+    --   require("monokai").setup({
+    --     palette = palette,
+    --     custom_hlgroups = {
+    --       Visual = {
+    --         bg = palette.base5,
+    --       },
+    --       VisualNOS = {
+    --         bg = palette.base5,
+    --       },
+    --       CursorLine = {
+    --         bg = palette.base4,
+    --       },
+    --       Comment = {
+    --         fg = palette.base7,
+    --         style = "italic",
+    --       },
+    --     },
+    --   })
+    -- end,
+  },
+  {
+    "projekt0n/github-nvim-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      local palette = require("monokai").pro
-      palette.white = '#C3C0C0'
-      palette.grey = '#72696A'
-      palette.black = "#000000"
-      palette.pink = '#D06A86'
-      palette.green = '#77C269'
-      palette.aqua = '#6CB5CC'
-      palette.yellow = '#E4C15A'
-      palette.orange = '#FC9867'
-      palette.purple = '#AB9DF2'
-      palette.red = '#D15454'
-      require("monokai").setup({
-        palette = palette,
-        custom_hlgroups = {
-          Visual = {
-            bg = palette.base5,
-          },
-          VisualNOS = {
-            bg = palette.base5,
-          },
-          CursorLine = {
-            bg = palette.base4,
-          },
-          Comment = {
-            fg = palette.base7,
-            style = "italic",
-          },
-        },
-      })
+      require("github-theme").setup({})
+
+      vim.cmd("colorscheme github_dark_dimmed")
     end,
   },
   -- {
-  --   "neanias/everforest-nvim",
-  --   version = false,
-  --   lazy = false,
-  --   priority = 1000, -- make sure to load this before all the other start plugins
-  --   -- Optional; default configuration will be used if setup isn't called.
-  --   config = function()
-  --     local everforest = require("everforest")
-  --     everforest.setup({
-  --       background = "hard",
-  --       transparent_background_level = 0.05,
-  --       italics = true,
-  --       disable_italic_comments = false,
-  --       colours_override = function(palette) end,
-  --       on_highlights = function(hl, palette)
-  --         hl.CurrentWord = { underline = true, bold = true }
-  --         local myblue = "#23c8db"   -- 蓝色浅一点
-  --         hl.InfoText = { fg = myblue, bg = myblue, sp = myblue, bold = true }
-  --       end,
-  --     })
-  --     everforest.load()
-  --   end,
-  -- },
-  -- {
   --   "LazyVim/LazyVim",
   --   opts = {
-  --     colorscheme = "everforest",  -- neo-tree展示不好看
+  --     colorscheme = "github_dark_dimmed", 
   --   },
   -- },
 }
