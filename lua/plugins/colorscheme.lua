a108656224d0b76a8d02fd69d7bc61b6ccf45f56
stormZhu <779724979@qq.com>
2024-02-18 23:13:48 +0800

    Update colorscheme.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -39,45 +39,54 @@ return {
     "Mofiqul/dracula.nvim",
     lazy = true,
     -- priority = 1000,
-    opts = {
-      colors = {
-        bg = "#292A35", --
-        fg = "#E7E7E0", -- 白色字体
-        selection = "#3A404D",
-        comment = "#70747f",
-        orange = "#FDC38E",
-        -- ANSI
-        black = "#1C1C1C", -- ANSI 0
-        red = "#DD6E6B",
-        green = "#ADDD9A",
-        yellow = "#E8EDA2",
-        purple = "#BAA0E8", -- used as ANSI 4 (blue)
-        pink = "#E48CC1",
-        cyan = "#A7DFEF",
-        white = "#F6F6F5", -- ANSI 7, 'selection' used for ANSI 8
-        -- indexes 9-15
-        bright_red = "#E1837F",
-        bright_green = "#97EDA2",
-        bright_yellow = "#F6F6B6",
-        bright_blue = "#D0B5F3",
-        bright_magenta = "#E7A1D7",
-        bright_cyan = "#BCF4F5",
-        bright_white = "#FFFFFF", -- index 15
-
-        menu = "#21222C",
-        visual = "#505663",
-        gutter_fg = "#4B5263",
-        nontext = "#3B4048",
-      },
-    },
+    -- opts = {
+    --   colors = {
+    --     bg = "#292A35", --
+    --     fg = "#E7E7E0", -- 白色字体
+    --     selection = "#3A404D",
+    --     comment = "#70747f",
+    --     orange = "#FDC38E",
+    --     -- ANSI
+    --     black = "#1C1C1C", -- ANSI 0
+    --     red = "#DD6E6B",
+    --     green = "#ADDD9A",
+    --     yellow = "#E8EDA2",
+    --     purple = "#BAA0E8", -- used as ANSI 4 (blue)
+    --     pink = "#E48CC1",
+    --     cyan = "#A7DFEF",
+    --     white = "#F6F6F5", -- ANSI 7, 'selection' used for ANSI 8
+    --     -- indexes 9-15
+    --     bright_red = "#E1837F",
+    --     bright_green = "#97EDA2",
+    --     bright_yellow = "#F6F6B6",
+    --     bright_blue = "#D0B5F3",
+    --     bright_magenta = "#E7A1D7",
+    --     bright_cyan = "#BCF4F5",
+    --     bright_white = "#FFFFFF", -- index 15
+    --
+    --     menu = "#21222C",
+    --     visual = "#505663",
+    --     gutter_fg = "#4B5263",
+    --     nontext = "#3B4048",
+    --   },
+    -- },
   },
+  { "mhartington/oceanic-next" },
+  { "lewis6991/github_dark.nvim" },
   {
-    "mhartington/oceanic-next",
+    "neanias/everforest-nvim",
+    config = function()
+      require("everforest").setup({
+        on_highlights = function(hl, palette)
+          hl.CurrentWord = { fg = palette.none, bg = palette.none, bold = true, underline = true }
+        end,
+      })
+    end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "OceanicNext",
+      colorscheme = "everforest",
     },
   },
 }
