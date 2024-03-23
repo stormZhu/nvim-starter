6532cb82ebbeb0eaaa4b7b1404b85523a6cbf08f
stormZhu <779724979@qq.com>
2024-03-24 02:25:28 +0800

    Create mark.lua

---
+++ lua/plugins/mark.lua
@@ -0,0 +1,10 @@
+return {
+  "lfv89/vim-interestingwords",
+}
+
+-- nnoremap <silent> <leader>k :call InterestingWords('n')<cr>
+-- vnoremap <silent> <leader>k :call InterestingWords('v')<cr>
+-- nnoremap <silent> <leader>K :call UncolorAllWords()<cr>
+--
+-- nnoremap <silent> n :call WordNavigation(1)<cr>
+-- nnoremap <silent> N :call WordNavigation(0)<cr>
