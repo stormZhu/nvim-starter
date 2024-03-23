569ea0b91e7bc6ae153c7da53e7a638fea702183
stormZhu <779724979@qq.com>
2024-03-23 20:03:57 +0800

    Update session.lua

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -3,13 +3,15 @@ return {
   event = "BufReadPre",
   opts = { 
     pre_save = function ()
-      require('neo-tree.sources.manager').close_all()
+      -- require('neo-tree.sources.manager').close_all()
+      vim.api.nvim_command("NvimTreeClose")
       -- vim.notify('closed all')
     end
   },
-  keys = {
-    { "<leader>qs", function() vim.cmd([[Neotree close]]); require("persistence").load() end, desc = "Restore Session" },
-    { "<leader>ql", function() vim.cmd([[Neotree close]]); require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+    keys = {
+  --   { "<leader>qs", function() vim.cmd([[Neotree close]]); require("persistence").load() end, desc = "Restore Session" },
+    { "<leader>qs", function() vim.cmd([[NvimTreeClose]]); require("persistence").load() end, desc = "Restore Session" },
+    { "<leader>ql", function() vim.cmd([[NvimTreeClose]]); require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
     { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
   },
 }
