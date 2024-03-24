fda9eb31f2987fc66b0cbdeba9b72b31873e823e
stormZhu <779724979@qq.com>
2024-03-24 10:41:54 +0800

    Update and rename mark.lua to editor.lua

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "mg979/vim-visual-multi",
+  },
+  {
+    -- nnoremap <silent> <leader>k :call InterestingWords('n')<cr>
+    -- vnoremap <silent> <leader>k :call InterestingWords('v')<cr>
+    -- nnoremap <silent> <leader>K :call UncolorAllWords()<cr>
+    -- nnoremap <silent> n :call WordNavigation(1)<cr>
+    -- nnoremap <silent> N :call WordNavigation(0)<cr>
+    "lfv89/vim-interestingwords",
+    keys = {
+      { "<leader>k", "<cmd>call InterestingWords('n')<cr>", desc = "HlToggle" },
+      { "<leader>K", "<cmd>call UncolorAllWords()<cr>", desc = "HlDismiss" },
+    },
+  },
+}
