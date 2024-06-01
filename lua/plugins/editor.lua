1cd2f613efc11a40fd80c803c33fa63a5ada8ae3
stormZhu <779724979@qq.com>
2024-06-01 19:25:04 +0800

    Update editor.lua

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -14,4 +14,28 @@ return {
       { "<leader>K", "<cmd>call UncolorAllWords()<cr>", desc = "HlDismiss" },
     },
   },
+  {
+    "sindrets/diffview.nvim",
+    event = "VeryLazy",
+    cmd = { "DiffviewOpen" },
+    opts = {
+      enhanced_diff_hl = true,
+      view = {
+        default = { winbar_info = true },
+        file_history = { winbar_info = true },
+      },
+      hooks = {
+        diff_buf_read = function(bufnr)
+          vim.b[bufnr].view_activated = false
+        end,
+      },
+    },
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    -- event = "LazyFile",
+    opts = {
+      current_line_blame = true,
+    },
+  }
 }
